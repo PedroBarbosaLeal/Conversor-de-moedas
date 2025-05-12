0cf0310febf50c71fd56ac159fd5c9eacc84398a
Pedro Leal <80418208+PedroBarbosaLeal@users.noreply.github.com>
2025-05-12 18:24:12 -0300

    Projeto finalizado

--- src/Conectar.java
+++ src/Conectar.java
@@ -4,32 +4,42 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 public class Conectar {
+    double infoMoeda = 0;
+    double infoMoeda2 = 0;
 
-        public void conectarClient(String valor) throws IOException, InterruptedException {
-            HttpClient client = HttpClient.newHttpClient();
+    public void conectarClient(String valor, String valor2) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
 
-            String end = "https://v6.exchangerate-api.com/v6/bd6eaba2d81bb6406c5b855b/latest/"+valor;
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(end))
-                    .build();
+        String end = "https://v6.exchangerate-api.com/v6/bd6eaba2d81bb6406c5b855b/latest/" + valor;
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(end))
+                .build();
 
-            HttpResponse<String> response = client
-                    .send(request, HttpResponse.BodyHandlers.ofString());
-            String json = response.body();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        String json = response.body();
 
-            System.out.println(json);
+        JsonObject jsonObject = JsonParser.parseString(json).getAsJsonObject();
+        JsonObject rates = jsonObject.getAsJsonObject("conversion_rates");
 
+        double moeda = rates.get(valor).getAsDouble();
+        infoMoeda = moeda;
+        double moeda2 = rates.get(valor2).getAsDouble();
+        infoMoeda2 = moeda2;
+        System.out.println("Cotação "+valor + ": " + moeda);
+        System.out.println("Cotação "+valor2 + ": " + moeda2);
 
-            JsonObject jsonObject = JsonParser.parseString(json).getAsJsonObject();
-            JsonObject rates = jsonObject.getAsJsonObject("conversion_rates");
+    }
 
-            double moeda = rates.get(valor).getAsDouble();
-            System.out.println("USD: $"+ moeda);
+    public double converterMoeda(double valor){
+        infoMoeda=valor;
+        return infoMoeda*infoMoeda2;
+    }
 
-        }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,73 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-
         Scanner scr = new Scanner(System.in);
-        System.out.println("Digite a moeda que você quer procurar");
-        String moeda = scr.nextLine();
-        System.out.println("Digite a moeda 2");
-
-
         Conectar conect = new Conectar();
+        int chave = 0;
 
-        conect.conectarClient(moeda);
+        do{
+            System.out.println("-----------------------------------------");
+            System.out.println("Seja bem-vindo(a) ao Conversor de Moeda:");
+            System.out.println("1) Dólar =>> Peso argentino");
+            System.out.println("2) Peso argentino =>> Dólar");
+            System.out.println("3) Dólar =>> Real brasileiro");
+            System.out.println("4) Real brasileiro =>> Dólar");
+            System.out.println("5) Dólar =>> Peso colombiano");
+            System.out.println("6) Peso colombiano =>> Dólar");
+            System.out.println("7) Sair");
+            System.out.println("Escolha uma opção válida:");
+            System.out.println("-----------------------------");
+            chave = scr.nextInt();
+            switch (chave){
+                case 1:
+                    System.out.println("qual o valor a ser convetido?");
+                    System.out.println("Digite:");
+                    double valor = scr.nextDouble();
+                    conect.conectarClient("USD","ARS");
+                    System.out.println(valor+" em $USD convertido em $ARS: "+conect.converterMoeda(valor));
+                    break;
 
+                case 2:
+                    System.out.println("qual o valor a ser convetido?");
+                    System.out.println("Digite:");
+                    double valor2 = scr.nextDouble();
+                    conect.conectarClient("ARS","USD");
+                    System.out.println(valor2+" em $ARS convertido em $USD: "+conect.converterMoeda(valor2));
+                    break;
+
+                case 3:
+                    System.out.println("qual o valor a ser convetido?");
+                    System.out.println("Digite:");
+                    double valor3 = scr.nextDouble();
+                    conect.conectarClient("USD","BRL");
+                    System.out.println(valor3+" em $USD convertido em R$BRL: "+conect.converterMoeda(valor3));
+                    break;
+                case 4:
+                    System.out.println("qual o valor a ser convetido?");
+                    System.out.println("Digite:");
+                    double valor4 = scr.nextDouble();
+                    conect.conectarClient("BRL","USD");
+                    System.out.println(valor4+" em $BRL convertido em R$USD: "+conect.converterMoeda(valor4));
+                    break;
+                case 5:
+                    System.out.println("qual o valor a ser convetido?");
+                    System.out.println("Digite:");
+                    double valor5 = scr.nextDouble();
+                    conect.conectarClient("USD","COP");
+                    System.out.println(valor5+" em $USD convertido em R$COP: "+conect.converterMoeda(valor5));
+                    break;
+                case 6:
+                    System.out.println("qual o valor a ser convetido?");
+                    System.out.println("Digite:");
+                    double valor6 = scr.nextDouble();
+                    conect.conectarClient("COP","USD");
+                    System.out.println(valor6+" em $COP convertido em R$USD: "+conect.converterMoeda(valor6));
+                    break;
+                case 7:
+                    System.out.println("Programa finalizado!");
+                default:
+                    System.out.println("Opção invalida!");
+            }
+        }while(chave!=7);
     }
 }
